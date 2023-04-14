c7b0aae23a8e3a1ddd05f7140c381e859732e6d5
abhay-rana <abhayrana286@gmail.com>
2023-04-14 22:55:01 +0530

    prettier

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import ComponentOne from "~/components/component-one";
+import ComponentOne from '~/components/component-one';
 // import viteLogo from "/vite.svg";
 
 function App() {
-	const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<div className="text-red-400">hello</div>
-		</>
-	);
+    return (
+        <>
+            <div>
+                <p className="m-2 h-4 w-4 bg-blue-400 p-4 text-red-400">
+                    hello
+                </p>
+            </div>
+        </>
+    );
 }
 
 export default App;
