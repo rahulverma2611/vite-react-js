61c0a83455d2764d0486efd7fc47a186eeb0d932
abhay-rana <abhayrana286@gmail.com>
2023-04-18 00:08:45 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { ClickToComponent } from 'click-to-react-component';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import store from '~/reducers';
 
 //for tailwind
 import App from './app';
@@ -8,7 +11,9 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
         {/* <ClickToComponent /> */}
     </React.StrictMode>
 );
